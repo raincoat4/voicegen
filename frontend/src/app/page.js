0d58541d596af0103a30ac48868ba71b05520e9d
raincoat4 <lr1tmrr@gmail.com>
2024-07-08 16:19:14 -0700

    feature: added link to source code

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,13 +1,17 @@
 'use client';
 import React from 'react';
 import Form from './components/Form';
+import github from '/Users/liamrogers/Documents/code/voicerecog/frontend/github.jpeg';
 
 function App() {
-
+  const reloadPage = () => {
+    window.location.reload();
+  };
   return (
     <>
-    <div className="navbar bg-base-100">
-      <a className="btn btn-ghost text-xl">Voice-Gen</a>
+    <div className="navbar bg-base-100 justify-between">
+      <a className="btn btn-ghost text-xl" onClick={reloadPage}>Voice-Gen</a>
+      <a className="btn btn-ghost text-l" href = "https://github.com/raincoat4/voicegen">Click here to view the source code!</a>
     </div>
     <div className = "absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
       <Form/>
